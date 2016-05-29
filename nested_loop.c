bd6b15a48ac1d244e80b3fa83449dc08a3cde69f
Lisheng Zhou <zhou@dls.rutgers.edu>
2016-05-29 15:20:10 -0400

    Create nested_loop.c

---
+++ nested_loop.c
@@ -0,0 +1,33 @@
+// Created by: Lisheng
+
+// This function will create nested loops with number of levels defined by the user
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "nested_loop.h"
+
+
+void nestedLoop ( int cvrg, int level, int* array )
+{
+	if ( level == 0 )
+	{
+		for ( int i = 0; i <= cvrg; i++ )
+		{
+			array[level] = i;
+
+			for ( int arrayi=0; arrayi<nloci; arrayi++)
+			{
+				printf("%i,", array[arrayi]);
+			}
+
+		}
+	} else {
+		for ( int prev_i = 0; prev_i <= cvrg; prev_i++ )
+		{
+			array[level] = prev_i;
+			nestedLoop( cvrg, level-1, array );
+		}
+	}
+}
